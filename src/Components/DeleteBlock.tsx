aa717da6b61b2c51b2ca7ef3041901a1444df400
EnzoCam7 <camerac09@gmail.com>
2023-11-14 17:28:39 -0300

    Feat: delete block component

---
+++ src/Components/DeleteBlock.tsx
@@ -0,0 +1,13 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faX } from '@fortawesome/free-solid-svg-icons'
+
+const DeleteBlock = () => {
+    return (
+        <FontAwesomeIcon 
+            icon={faX}
+            className='text-red-500 hover:cursor-pointer hover:text-red-700'
+        />
+    )
+}
+
+export default DeleteBlock
