0b75ff2a100718d0c4adf446a9ae98529eaaf24b
EnzoCam7 <camerac09@gmail.com>
2023-11-15 19:22:36 -0300

    Component

---
+++ src/Components/PriorityDisplay.tsx
@@ -0,0 +1,36 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFire } from "@fortawesome/free-solid-svg-icons";
+
+const PriorityDisplay = () => {
+  return (
+    <div className="flex justify-start">
+      <FontAwesomeIcon
+        icon={faFire}
+        className="text-red-600 text-base"
+      />
+      
+      <FontAwesomeIcon
+        icon={faFire}
+        className="text-red-600 text-base"
+      />
+      
+      <FontAwesomeIcon
+        icon={faFire}
+        className="text-red-600 text-base"
+      />
+      
+      <FontAwesomeIcon
+        icon={faFire}
+        className="text-red-600 text-base"
+      />
+      
+      <FontAwesomeIcon
+        icon={faFire}
+        className="text-red-600 text-base"
+      />
+
+    </div>
+  );
+};
+
+export default PriorityDisplay;
