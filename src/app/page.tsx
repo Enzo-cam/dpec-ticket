20c8138d0368efcd62147bcdc5088452f08c2c4e
EnzoCam7 <camerac09@gmail.com>
2023-11-20 19:42:58 -0300

    Feat: get tickets and display them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,52 @@
+"use client"
+import {useState, useEffect} from 'react'
+import TicketCard from "@/Components/TicketCard";
+import { ITicket } from "@/Interfaces/ITicket";
 
-export default function Home() {
-  return(
-    <h1>Hola</h1>
-  )
-}
+const getTickets = async (): Promise<ITicket[] | undefined> => {
+  try {
+    const res = await fetch("/api/ticket", {
+      cache: "no-store",
+    });
+    return res.json();
+  } catch (error) {
+    console.log("Fallo en obtener los tickets: ", error);
+  }
+};
+
+const Home = () => {
+  const [tickets, setTickets] = useState<ITicket[]>([]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const fetchedTickets = await getTickets();
+      if (fetchedTickets) {
+        setTickets(fetchedTickets);
+      }
+    };
+
+    fetchTickets();
+  }, []);
+
+
+  // const uniqueCategories = [
+  //   ...new Set(tickets?.map(({ category }) => category)),
+  // ];
+
+  console.log("Probando")
+  console.log(tickets)
+
+  return (
+    <div className="p-5">
+      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+        <TicketCard />
+
+        <TicketCard />
+        <TicketCard />
+        <TicketCard />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
